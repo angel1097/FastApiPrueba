9a8268b5ec3139551637cf010df1f4ee807fb653
Angel Leobardo Perez Perez <angelperez7246@gmail.com>
2025-01-15 13:36:04 -0600

    bd commit3

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import String,Integer,Column
 from database import Base
 
 class Ingreso(Base):
-    __tablename__="registrodeIngreso"
+    __tablename__="registrodeingreso"
     idregistro=Column(Integer,primary_key=True,index=True)
     documentoingreso=Column(String(100))
     nombrepersona=Column(String(100))
