87fc7a17b6d9357167d3fac5018eb3af9393e6dc
Angel Leobardo Perez Perez <angelperez7246@gmail.com>
2025-01-15 13:33:33 -0600

    bd commit2

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:yvgaFYLdXaFEmnvLVCKTglnvXRUKPzNl@monorail.proxy.rlwy.net:14199/ejemplobd2"
+URL_DATABASE = "mysql+pymysql://root:yvgaFYLdXaFEmnvLVCKTglnvXRUKPzNl@monorail.proxy.rlwy.net:14199/railway"
 engine= create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
